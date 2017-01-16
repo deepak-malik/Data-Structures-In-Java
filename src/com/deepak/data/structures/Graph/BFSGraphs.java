69453b0f659186ac6f2d18786fa0c057e28f6c0d
Deepak Malik <deepak.malik174266@gmail.com>
2017-01-15 21:29:55 -0800

    Algorithms for BFS and DFS

--- src/com/deepak/data/structures/Graph/BFSGraphs.java
+++ src/com/deepak/data/structures/Graph/BFSGraphs.java
@@ -1,5 +1,21 @@
+/**
+ * Data-Structures-in-Java
+ * BFSGraphs.java
+ */
 package com.deepak.data.structures.Graph;
 
+/**
+ * Breadth First Search implemented for Graphs
+ * 
+ * <p> Algorithm : 
+ * 	1. Push the root node in the Queue.
+ * 	2. Loop until the Queue is empty.
+ * 	3. Remove the Node from the Queue.
+ * 	4. If the removed node has unvisited children, mark them as visited and insert the unvisited children in the Queue. 
+ * </p>
+ *  
+ * @author Deepak
+ */
 public class BFSGraphs {
 
 }
--- src/com/deepak/data/structures/Graph/DFSGraphs.java
+++ src/com/deepak/data/structures/Graph/DFSGraphs.java
@@ -1,5 +1,21 @@
+/**
+ * Data-Structures-in-Java
+ * DFSGraphs.java
+ */
 package com.deepak.data.structures.Graph;
 
+/**
+ * Depth First Search implemented for graphs
+ * <p> Algorithm :
+ * 	1. Push the root element to Stack
+ * 	2. Loop until stack is empty
+ * 	3. Peek the node from the Stack
+ * 	4. If the node has unvisited child nodes, get those nodes, push to Stack and mark as visited
+ * 	5. If the node does not have any unvisited child node, pop it from the Stack.   
+ * </p>
+ * 
+ * @author Deepak
+ */
 public class DFSGraphs {
 
 }

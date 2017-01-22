d6f0bfb476a3ecc757aaf89a0d2b294ad81c2268
Deepak Malik <deepak.malik174266@gmail.com>
2017-01-22 01:12:54 -0800

    Array based stack

--- src/com/deepak/data/structures/Stack/ArrayBasedStack.java
+++ src/com/deepak/data/structures/Stack/ArrayBasedStack.java
@@ -7,29 +7,11 @@ package com.deepak.data.structures.Stack;
 import java.util.NoSuchElementException;
 
 /**
- * Class implementing Stack based on Array
+ * Array based implementation of stack
+ * 
+ * @author Deepak
  */
 public class ArrayBasedStack {
-
-	/**
-	 * Sample implementation of stack
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		ArrayBasedStack stack = new ArrayBasedStack(5);
-		System.out.println("Creating a fresh Stack");
-		System.out.println("Size of Stack => " + stack.size());
-		System.out.println("Is Stack Empty => " + stack.isEmpty());
-		System.out.println("Inserting two items in Stack.");
-		stack.push(10);
-		stack.push(20);
-		System.out.println("After Insertion, Size of Stack => " + stack.size());
-		System.out.println("After Insertion, Is Stack Empty => " + stack.isEmpty());
-		System.out.println("Removing an item from Stack.");
-		stack.pop();
-		System.out.println("After Removal, Size of Stack => " + stack.size());
-		System.out.println("Top element on Stack => " + stack.peek());
-	}
 
 	/**
 	 * Array of objects representing Stack
@@ -43,6 +25,7 @@ public class ArrayBasedStack {
 
 	/**
 	 * Constructor to create a new Stack
+	 * 
 	 * @param capacity
 	 */
 	public ArrayBasedStack(int capacity) {
@@ -51,6 +34,7 @@ public class ArrayBasedStack {
 
 	/**
 	 * Method to push a item on top of stack
+	 * 
 	 * @param item
 	 */
 	public void push(Object item) {
@@ -62,30 +46,36 @@ public class ArrayBasedStack {
 
 	/**
 	 * Method to remove a item from top of stack
+	 * 
 	 * @return {@link Object}
 	 */
 	public Object pop() {
 		if (size == 0) {
 			throw new NoSuchElementException("Cannot delete from a empty Stack");
 		}
-		Object result = array[size - 1];
-		array[size--] = null;
+		Object result = array[0];
+		for (int i = 0; i < array.length - 1; i++) {
+			array[i] = array[i + 1];
+		}
+		size--;
 		return result;
 	}
 
 	/**
 	 * Method to look up top item in stack
+	 * 
 	 * @return {@link Object}
 	 */
 	public Object peek() {
 		if (size == 0) {
 			throw new NoSuchElementException("Cannot peek from a empty Stack");
 		}
-		return array[size - 1];
+		return array[0];
 	}
 
 	/**
 	 * Method to check size of the stack
+	 * 
 	 * @return {@link int}
 	 */
 	public int size() {
@@ -94,6 +84,7 @@ public class ArrayBasedStack {
 
 	/**
 	 * Method to check if stack is empty
+	 * 
 	 * @return {@link boolean}
 	 */
 	public boolean isEmpty() {
